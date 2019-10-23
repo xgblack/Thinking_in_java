8aad27d2ce5b05bee2b72dafc6c04b123dc13979
xgblack <gmg@xgblack.cn>
2019-10-23 22:09:25 +0800

    优先级

---
+++ concurrency/src/c21_2/SimplePriorities.java
@@ -0,0 +1,54 @@
+package c21_2;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author 小光
+ * @date 2019/10/23 21:57
+ * className: SimplePriorities
+ * description:
+ * ***************************************************************************
+ * Copyright(C),2018-2019,https://blog.xgblack.cn  .All rights reserved.
+ * ***************************************************************************
+ */
+public class SimplePriorities implements Runnable {
+    private int countDown = 5;
+    private volatile double d;
+    private int priority;
+
+    public SimplePriorities(int priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return Thread.currentThread() + ": " + countDown;
+    }
+
+    @Override
+    public void run() {
+        Thread.currentThread().setPriority(priority);
+        while (true) {
+            for (int i = 0; i < 100000; i++) {
+                d += (Math.PI + Math.E) / (double) i;
+                if (i % 1000 == 0) {
+                    Thread.yield();
+                }
+            }
+            System.out.println(this);
+            if (--countDown == 0) {
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new SimplePriorities(Thread.MIN_PRIORITY));
+        }
+        exec.execute(new SimplePriorities(Thread.MAX_PRIORITY));
+        exec.shutdown();
+    }
+}
