3a56ff37a368ae5d8252e6ac7ccdd9e845ee4dec
xgblack <gmg@xgblack.cn>
2019-10-29 10:03:42 +0800

    被互斥锁阻塞

---
+++ concurrency/src/c21_4/Interrupting2.java
@@ -0,0 +1,49 @@
+package c21_4;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author 小光
+ * @date 2019/10/29 9:54
+ * className: Interrupting2
+ * description:
+ * ***************************************************************************
+ * Copyright(C),2018-2019,https://blog.xgblack.cn  .All rights reserved.
+ * ***************************************************************************
+ */
+class BlockedMutex{
+    private Lock lock = new ReentrantLock();
+    public BlockedMutex(){
+        lock.lock();
+
+    }
+    public void f(){
+        try {
+            lock.lockInterruptibly();
+            System.out.println("lock acquired in f()");
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted from lock acquisition in f()");
+        }
+    }
+}
+
+class Blocked2 implements Runnable {
+    BlockedMutex blocked = new BlockedMutex();
+    @Override
+    public void run() {
+        System.out.println("Waiting for f() in BlockedMutex");
+        blocked.f();
+        System.out.println("Broken out of blocked call");
+    }
+}
+public class Interrupting2 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new Blocked2());
+        t.start();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Issuing t.interrupt()");
+        t.interrupt();
+    }
+}
