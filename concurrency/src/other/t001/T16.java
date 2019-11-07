ebe2b0fdc812030de0c99856400061188689e79c
xgblack <gmg@xgblack.cn>
2019-11-07 16:44:25 +0800

    锁的粒度

---
+++ concurrency/src/other/t001/T16.java
@@ -0,0 +1,45 @@
+package other.t001;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author 小光
+ * @date 2019/11/7 16:40
+ * className: T16
+ * description:
+ * ***************************************************************************
+ * Copyright(C),2018-2019,https://blog.xgblack.cn  .All rights reserved.
+ * ***************************************************************************
+ */
+public class T16 {
+    int count = 0;
+    synchronized void m1(){
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        count++;
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void m2(){
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        synchronized (this) {
+            count++;
+        }
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
