98a3db513b1e680c058dd1a22ee0d234af8f2fd7
xgblack <gmg@xgblack.cn>
2019-11-10 23:41:05 +0800

    生产者消费者

---
+++ concurrency/src/other/yxxy_021/MyContainer1.java
@@ -0,0 +1,73 @@
+package other.yxxy_021;
+
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author 小光
+ * @date 2019/11/10 21:32
+ * className: MyContainer1
+ * description:
+ * ***************************************************************************
+ * Copyright(C),2018-2019,https://blog.xgblack.cn  .All rights reserved.
+ * ***************************************************************************
+ */
+public class MyContainer1<T> {
+    final private LinkedList<T> list = new LinkedList<>();
+    final private int MAX = 10;
+    private int count = 0;
+
+    public synchronized void put(T t) {
+        while (count == MAX) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            list.add(t);
+            ++count;
+            this.notifyAll();
+        }
+    }
+
+    public synchronized T get(){
+        while (count == 0) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        T t = list.removeFirst();
+        count--;
+        this.notifyAll();
+        return t;
+    }
+
+    public static void main(String[] args) {
+        MyContainer1<String> c = new MyContainer1<>();
+        //启动消费者线程
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                for (int j = 0; j < 5; j++) {
+                    System.out.println(c.get());
+                }
+            },"c" + i).start();
+        }
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //启动生产者
+        for (int i = 0; i < 2; i++) {
+            new Thread(()->{
+                for (int j = 0; j < 25; j++) {
+                    c.put(Thread.currentThread().getName() + "" + j);
+                }
+            },"p" + i).start();
+        }
+    }
+}
